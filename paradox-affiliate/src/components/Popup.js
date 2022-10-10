b3be7ecb78bbe9b87e8031b10b455660b9dc2ffe
paradoxevents <media@paradoxevents.co.uk>
2022-10-10 19:25:03 +0100

    popup update

--- paradox-affiliate/src/components/Popup.js
+++ paradox-affiliate/src/components/Popup.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./Popup.css"
+import logo from "../image/paraffiliate.png";
 
 function Popup(props) {
   return (props.trigger) ? (
     <div className='popup'>
       <div className="popup-inner">
+      <img src={logo} className="app-logo"/>
         <button className="close-btn" onClick={()=> props.setTrigger(false)}>close</button>
         { props.children }
+        
       </div>
     </div>
   ) : ""; 
