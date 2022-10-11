350d93564868b9634c83d2de9e9db3adf796c476
paradoxevents <media@paradoxevents.co.uk>
2022-10-11 12:05:08 +0100

    timed popup update

--- paradox-affiliate/src/App.js
+++ paradox-affiliate/src/App.js
@@ -6,6 +6,7 @@ import { Subtitle2 } from './components/Subtitle2';
 import { Title } from './components/Title';
 import Popup from './components/Popup';
 import { useState, useEffect} from "react";
+import tglogo from './image/telegram_darwin.png';
 
 function App() {
   const [buttonPopup, setButtonPopup] = useState(false);
@@ -30,17 +31,27 @@ function App() {
        
         <buttom onClick={() =>setButtonPopup(true)} className="submit-button">SUBMIT!</buttom>
         <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-          <h3>Congratualations!</h3>
-        </Popup>
-        
-        <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
-          <img scr="./image/paraffiliate.png"/> 
-          <h3>Congratualations!</h3>
-          <p>Youve decided to make money by joining the 
+        <h3 className="popup-heading">Congratulations!</h3>
+          <p className="popup-content">Youve decided to make money by joining the 
   Paradox
 Affiliate Program!
 
 Join Our Paradox Affiliate Telegram group for more info!</p>
+        </Popup>
+        
+        <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
+          <img scr="./image/paraffiliate.png"/> 
+          <h3 className="popup-heading">Congratulations!</h3>
+          <div className="popup-content">
+          <p>
+          <span>For Joining Up The</span><br/>
+          <span>Paradox</span><br/>
+          <span>Affiliate Program!</span>
+          </p>
+          <p>Every 24 hours Your Payment will be submitted Join Our Paradox Affiliate Telegram group for more info!</p>
+          </div>
+          <img src={tglogo} className="TG-logo" />
+
         </Popup>
         <Footer></Footer>
         </main>
